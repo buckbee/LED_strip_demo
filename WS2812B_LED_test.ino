3514c2d56c29b1ce473fbfeb4a8924d374ee0169
Kasper Buckbee <kasper.buckbee@gmail.com>
2017-09-29 19:06:27 +0100

    Update WS2812B_LED_test.ino

--- WS2812B_LED_test.ino
+++ WS2812B_LED_test.ino
@@ -7,38 +7,7 @@
 ** Edited by: -
 */
 
-// NOTES:
-// 1) To define a LED strip: 
-//      Adafruit_NeoPixel strip = Adafruit_NeoPixel(param1, param2, param3);
-// Where:
-//      Parameter 1 = number of pixels in strip
-//      Parameter 2 = pin number (most are valid)
-//      Parameter 3 = pixel type flags, add the following together as needed:
-//
-//        NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//        NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//        NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//        NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-// Example:
-// Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 
-// 2) To set LED colours:
-//      a) ledA.setPixelColor(n, red, green, blue);
-//      b) ledA.setPixelColor(n, color); 
-// Where:
-//      n = pixel number along the strip (numbered 0-9 for 10 pixels)
-//      color = 32 bit value that combines RGB values as one number
-//      red, green, blue: any value between 0 to 255
-
-// 3) Can also save colours: 
-//       uint32_t magenta = strip.Color(255, 0, 255);
-//    and then just pass in 'magenta' to the 'color' variable.
-
-// 4) setPixelColor() does not have an immediate effect on the LEDs. 
-//    To “push” the color data to the strip, call .show()
-
-
-// * CODE STARTS *
 //Include needed libraries
 #include <Adafruit_NeoPixel.h>
 
@@ -58,7 +27,7 @@ void setup() {
 void loop() {
 
   //Set 10 pixels to different colours
-  strip.setPixelColor(0, 255, 0, 255);
+  strip.setPixelColor(0, 255, 0, 255); // setPixelColor(Pin#, R, G, B)
   //Blank: ________________________//
   //Blank: ________________________//
   //Blank: ________________________//
